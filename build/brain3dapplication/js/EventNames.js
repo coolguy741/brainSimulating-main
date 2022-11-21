2d0d1e80797c8d32d49945077991cfe1090783a1
reinforest915820 <alexandre915820@gmail.com>
2022-11-20 23:35:08 -0500

    contain build files

---
+++ build/brain3dapplication/js/EventNames.js
@@ -0,0 +1,7 @@
+var EventNames = function() {  
+   this.LOAD_PROGRESS = "LOAD_PROGRESS";
+   this.LOAD_COMPLETE = "LOAD_COMPLETE";
+   this.LOAD_ERROR = "LOAD_ERROR";
+}
+EventNames = new EventNames();
+
