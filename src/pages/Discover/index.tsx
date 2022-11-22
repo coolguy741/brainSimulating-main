eb0aa53dfc77820f5395719c83849149eab243d6
reinforest915820 <alexandre915820@gmail.com>
2022-11-22 15:08:36 -0500

    fix: slide 08

--- src/pages/Discover/index.tsx
+++ src/pages/Discover/index.tsx
@@ -14,7 +14,7 @@ const Wrapper = styled.div`
     }
 
     button {
-        font-size: 2rem !important;
+        font-size: 1rem !important;
     }
 `
 
@@ -29,7 +29,7 @@ export const Discover = ({ setSubName }: any) => {
         <Wrapper className={`fixed top-0 left-0 z-50 w-screen h-screen flex flex-col p-6 bg-transparent`}>
             <div className="mt-auto mb-auto">
                 <div className="l-container">
-                    <p className="text-5xl font-medium text-white leading-[4rem] z-50">Did you know that watching porn frequently can other areas of your life?</p>
+                    <p className="text-5xl font-medium text-white leading-[4rem] z-50">Did you know that watching porn regularly can affect several areas of your life?</p>
                     <ArrowButton onClickCallback={ onClickDiscover }>
                         FIND OUT MORE
                     </ArrowButton>
