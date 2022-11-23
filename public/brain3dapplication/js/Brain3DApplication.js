845d2dd711b53fcfa077905f2e17c258cd44b75d
devofficer <myeyesontech@gmail.comeval>
2022-11-23 06:38:05 -0800

    [feat] brain glow

--- public/brain3dapplication/js/Brain3DApplication.js
+++ public/brain3dapplication/js/Brain3DApplication.js
@@ -16,12 +16,11 @@ function Brain3DApplication() {
     this.question9Score = 0;
     this.question10Score = 0;
 
-    this.weightHigh = 54;
-    this.weightMedium = 33;   
-    this.weightLow = 13;
+    this.weightHigh = 50;
+    this.weightMedium = 30;
+    this.weightLow = 0;
 
     this.influence = 0;
-
 }
 
 Brain3DApplication.prototype = {
@@ -36,9 +35,6 @@ Brain3DApplication.prototype = {
 
     createBrainSectionData: function () {
         this.brainSectionData = {};
-
-
-
         var sectionNames = ["amygdala", "hippocampus", "nucleus_accumbens", "striatum", "medial_prefrontal_cortex", "dorsolateral_prefrontal_cortex"]
         var sectionColors = ["#C6AF22", "#25712F", "#12E7D7", "#FA7F00", "#28CE3E", "#C6AF22"];       
         var handleNodeVisibilityWithEffect = [false, false, false, false, true, true];
@@ -66,43 +62,46 @@ Brain3DApplication.prototype = {
     },
 
 
-    // form calls this function with answer 18-95
     question1Sum: function (value) {
-        this.question1Score = 13 * ((100 - (((value - 18) / 77) * 100)) / 100);
+        if (value <= 35) this.question1Score = 1 - (0.2 * (value - 18)) / 17;
+        else this.question1Score = 0.8 - 0.5 * [(value - 35) / 55] ** 2;
         this.calculateTotalAndApply();
-    },    
+    },
 
-    // form calls this function with answer 5-25
+    // form calls this function with answer 5-30
     question2Sum: function (value) {
-        this.question3Score = 18 * ((100 - (((value - 5) / 20) * 100)) / 100);
+        this.question2Score = 1 - (value - 5) / 25;
         this.calculateTotalAndApply();
     },
 
     // form calls this function with answer 0-40
     question3Sum: function (value) {
-        this.question4Score = 18 * (value / 40)
+        this.question3Score = value / 15;
         this.calculateTotalAndApply();
     },
 
-     // form calls this function with answer 1 or 0.5 or 0
-     question4Sum: function (value) {
-        this.question7Score = 15 * value;
+    // form calls this function with answer 1 or 0.5 or 0
+    question4Sum: function (value) {
+        this.question4Score = 1 - value * 0.5;
         this.calculateTotalAndApply();
     },
 
     // form calls this function with answer 1 or 0.5 or 0
     question5Sum: function (value) {
-        this.question5Score = 18 * value;
+        this.question5Score = value;
         this.calculateTotalAndApply();
     },
 
     calculateTotalAndApply: function () {
-        var wPercentHigh = ((this.question2Score + this.question3Score + this.question5Score ) / this.weightHigh) * this.weightHigh;      
-        var wPercentMed = ((this.question4Score) / this.weightMedium) * this.weightMedium;       
-        var wPercentLow = ((this.question1Score ) / this.weightLow) * this.weightLow;
-        var percentTotal = wPercentHigh +  wPercentMed +  wPercentLow;
-        this.updateBrainSection(percentTotal / 100);
+        this.updateBrainSection(
+            this.question1Score,
+            this.question2Score,
+            this.question3Score,
+            this.question4Score,
+            this.question5Score
+        );
     },
+    
     updateBrainZoom(zoomValue,animationTime){
         if(animationTime){
             gsap.to(this.babylonScene.camera,{radius:zoomValue,duration:animationTime,ease:Power2.easeOut})
@@ -160,40 +159,88 @@ Brain3DApplication.prototype = {
         }
     },
 
-
-
-    updateBrainSection: function (value) {
-       
+    updateBrainSection: function (s1, s2, s3, s4, s5) {
+        value = (s1 + s2 + s3 + s4 + s5) / 5;
+    
+        if (this.babylonScene.brainIdleAnimation) {
+          this.babylonScene.brainIdleAnimation.revert();
+        }
+        if (this.babylonScene.questionParticles) {
+          this.babylonScene.questionParticles.stop();
+          this.babylonScene.questionParticles.dispose();
+        }
         if (!this.ready) return;
         this.influence = value;
-        var duration = 0.175 - (0.125 * (this.influence));
+        var duration = 0.175 - (0.125 * (s2 + s3 + s5) * s1 * s4) / 3;
         var overlap = duration * 0.9;
         var overlapString = "-=" + overlap;
         if (this.glowTween) {
-            this.glowTween.kill();
+          this.glowTween.kill();
         }
         this.glowTween = gsap.timeline({ repeat: -1 });
+        console.log("brainSelection", this.brainSectionData);
         for (var prop in this.brainSectionData) {
-            let item = this.brainSectionData[prop];
-            item.material.emissiveIntensity = this.influence * 0.1;
-            if (item.handleNodeVisibilityWithEffect) {
-                item.material.alpha = 0;
-                this.glowTween.to(item.material, { alpha: 0.8, emissiveIntensity: this.influence * item.emmissiveScalar, duration: duration, ease: Linear, yoyo: true, repeat: 1 }, overlapString)
-            } else {
-                this.glowTween.to(item.material, { emissiveIntensity: this.influence * item.emmissiveScalar, duration: duration, ease: Linear, yoyo: true, repeat: 1 }, overlapString)
-            }
-
-
+          // "amygdala",
+          // "hippocampus",
+          // "nucleus_accumbens",
+          // "striatum",
+          // "medial_prefrontal_cortex",
+          // "dorsolateral_prefrontal_cortex",
+          let item = this.brainSectionData[prop];
+    
+          let brightness = 0;
+          if (
+            prop == "amygdala" ||
+            prop == "hippocampus" ||
+            prop == "nucleus_accumbens"
+          ) {
+            brightness = 0.5 + (s2 + s3 + s5) / 6;
+          } else if (prop == "striatum") {
+            brightness = 0.002 + (s5 ** 3 * 0.99 + (s2 + s3) * 0.005) * s1 * s4;
+          } else if (prop == "medial_prefrontal_cortex") {
+            brightness = 0.002 + (s3 ** 3 * 0.99 + (s2 + s5) * 0.005) * s1 * s4;
+          } else if (prop == "dorsolateral_prefrontal_cortex") {
+            brightness = 0.002 + (s2 ** 3 * 0.99 + (s3 + s5) * 0.005) * s1 * s4;
+          }
+          console.log(s1, s2, s3, s4, s5);
+    
+          item.material.emissiveIntensity = this.influence * 0.1;
+          if (item.handleNodeVisibilityWithEffect) {
+            item.material.alpha = 0;
+            this.glowTween.to(
+              item.material,
+              {
+                alpha: 0.8,
+                emissiveIntensity: brightness * item.emmissiveScalar * 0.5,
+                duration: duration,
+                ease: Linear,
+                yoyo: true,
+                repeat: 1,
+              },
+              overlapString
+            );
+          } else {
+            this.glowTween.to(
+              item.material,
+              {
+                emissiveIntensity: brightness * item.emmissiveScalar * 0.5,
+                duration: duration,
+                ease: Linear,
+                yoyo: true,
+                repeat: 1,
+              },
+              overlapString
+            );
+          }
         }
-
+    
         var effectScalar = 40;
         if (this.babylonScene.t2) {
-            this.babylonScene.t2.timeScale(1 + (this.influence * effectScalar))
+          this.babylonScene.t2.timeScale(1 + this.influence * effectScalar);
         }
-        this.babylonScene.glowLayer.blurKernelSize = 16 + (48 * this.influence);
-    }
-
-}
+        this.babylonScene.glowLayer.blurKernelSize = 16 + 48 * this.influence;
+    },
+};
 
 var inherit_1 = Object.create(PublishSubscribe.prototype);
 Object.assign(inherit_1, Brain3DApplication.prototype);
