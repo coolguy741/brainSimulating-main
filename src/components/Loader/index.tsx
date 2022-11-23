413561155fcf9165847365401268b88f85567ed5
devofficer <myeyesontech@gmail.comeval>
2022-11-22 16:53:06 -0800

    [feat] loader positioning

--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,8 +1,8 @@
 
 export const Loader = () => {
     return (
-        <div className="flex justify-center mt-20 z-10">
-            <img className="w-[70%] sm:w-[60%] md:w-[45%]" alt="pic" src="/assets/images/logo_loading.gif" />
+        <div className="flex flex-col items-center justify-center h-screen z-10">
+            <img className="-mt-8 w-[75%] sm:w-[55%] md:w-[45%]" alt="pic" src="/assets/images/logo_loading.gif" />
 
             {/* <h1>how does porn change your brain?</h1> */}
         </div>
