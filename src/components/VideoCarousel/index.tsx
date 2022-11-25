6b592cf78b0534a9b72068a78d40d3d83733c8c5
devofficer <myeyesontech@gmail.comeval>
2022-11-25 15:47:49 -0800

    [feat] new icons

--- src/components/VideoCarousel/index.tsx
+++ src/components/VideoCarousel/index.tsx
@@ -1,16 +1,12 @@
-import { useRef, useState, useEffect } from "react"
-import styled from "styled-components"
-import { Swiper, SwiperSlide, useSwiper } from 'swiper/react';
-import { Navigation, Pagination, EffectCoverflow } from "swiper";
+import { useEffect } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import styled from "styled-components";
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
-import 'swiper/css/effect-coverflow';
-import './index.scss'
-import RadioSelect from "../RadioGroup";
 import { ArrowButton } from "../../theme/components";
-import { useNavigate, Link } from "react-router-dom";
-import gsap from "gsap";
+import './index.scss';
 
 const Wrapper = styled.div`
     width: 40%;
@@ -24,38 +20,6 @@ const Wrapper = styled.div`
         font-weight: 900;
     }
 `
-const RotateIcon = styled.div`
-    .love-icon {
-        transform: rotateY(0deg);
-        animation: rotateAnimation1 4s linear infinite;
-    }
-
-    .cake-icon {
-        transform: rotateY(30deg);
-        animation: rotateAnimation2 4s linear infinite;
-    }
-
-    .sport-icon {
-        transform: rotateY(60deg);
-        animation: rotateAnimation3 4s linear infinite;
-    }
-
-    @keyframes rotateAnimation1 {
-        from {transform: rotateY(0deg);}
-        to {transform: rotateY(360deg);}
-    }
-    
-    @keyframes rotateAnimation2 {
-        from {transform: rotateY(30deg);}
-        to {transform: rotateY(390deg);}
-    }
-    
-    @keyframes rotateAnimation3 {
-        from {transform: rotateY(60deg);}
-        to {transform: rotateY(420deg);}
-    }
-`
-
 
 export const VideoCarousel = () => {
     const navigate = useNavigate()
@@ -72,17 +36,13 @@ export const VideoCarousel = () => {
     useEffect(() => {
         const brain3DApplication = (window as any).brain3DApplication
         brain3DApplication.turnOffLight();
-        let tl = gsap.timeline({});
-        tl.to(".love-icon", {repeat: -1, duration: 1,  y:360, ease: Power0.easeNone});
     }, [])
 
     return (
         <Wrapper className={`fixed top-0 left-0 z-50 w-screen h-screen flex flex-col justify-center items-center p-6`}>
             <div className="container | relative text-start text-white mt-32">
-                <div className="flex mb-16">
-                    <RotateIcon className="flex-none">
-                        <img className="love-icon w-24 h-20" src="/assets/images/love.png" alt = "love" />
-                    </RotateIcon>
+                <div className="flex items-center mb-16">
+                    <img className="w-32 h-32" src="/assets/images/love.gif" alt = "love" />
                     <div className="ml-6 flex-inital w-64">
                         <h5 className="text-xl pb-3 font-bold" style={{fontSize: "40px"}}>
                             INTIMACY
@@ -92,10 +52,8 @@ export const VideoCarousel = () => {
                         </p>
                     </div>
                 </div>
-                <div className="flex mb-16">
-                    <RotateIcon className="flex-none">
-                        <img className="cake-icon w-24 h-20" src="/assets/images/cake.png" alt = "cake" />
-                    </RotateIcon>
+                <div className="flex items-center mb-16">
+                    <img className="w-32 h-32" src="/assets/images/cake.gif" alt = "cake" />
                     <div className="ml-6 flex-inital" style={{width: "200px"}}>
                         <h5 className="text-xl pb-3 font-bold" style={{fontSize: "40px"}}>
                         TREATS
@@ -105,10 +63,8 @@ export const VideoCarousel = () => {
                         </p>
                     </div>
                 </div>
-                <div className="flex mb-8">
-                    <RotateIcon className="flex-none">
-                        <img className="sport-icon w-24 h-20" src="/assets/images/sport.png" alt = "cake" />
-                    </RotateIcon>
+                <div className="flex items-center mb-8">
+                    <img className="w-32 h-32" src="/assets/images/sport.gif" alt = "cake" />
                     <div className="ml-6 flex-inital" style={{width: "200px"}}>
                         <h5 className="text-xl pb-3 font-bold" style={{fontSize: "40px"}}>
                             EXERCISE
