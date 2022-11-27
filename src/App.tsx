5739b3cd24b2bf5e665d5ba61255dd3118e34448
devofficer <myeyesontech@gmail.comeval>
2022-11-27 12:54:15 -0800

    [feat] scroll icon and slide show:

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,8 @@ import { Route, Routes } from 'react-router-dom';
 import Pages from './pages';
 
 function App() {
-  const updateCursor = (e: any)=> {
-    const mouseX = e.clientX - 12;
-    const mouseY = e.clientY + 4;
-    const cursor: any =  document.querySelector('.mouse_scroll');
-    if(!cursor) return;
-    cursor.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  }
-    
   return (
-    <div className="App" onMouseMove={updateCursor}>
-      <div className="mouse_scroll">
-          <div className="mouse">
-            <div className="wheel"></div>
-          </div>
-          <div>
-            <span className="m_scroll_arrows unu"></span>
-            <span className="m_scroll_arrows doi"></span>
-            <span className="m_scroll_arrows trei"></span>
-          </div>
-      </div>
+    <div className="App">
       <Routes>
         <Route path="/*" element={<Pages />} />
       </Routes>
--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -6,12 +6,24 @@ const Pagination = ({ total } : any) => {
     const [ curPage ] = pageContext;
     const items = Array(total).fill(0);
     return (
-        <div className="w-full flex space-x-2 items-center justify-center absolute bottom-12">
+        <div className="w-full flex flex-col space-y-2 items-center justify-center absolute bottom-12">
+            {
+                curPage < total - 1 && <div className="mouse_scroll">
+                    <div className="mouse">
+                        <div className="wheel"></div>
+                    </div>
+                    <div>
+                        <span className="m_scroll_arrows unu"></span>
+                        <span className="m_scroll_arrows doi"></span>
+                        <span className="m_scroll_arrows trei"></span>
+                    </div>
+                </div>
+            }   
             
-            <div className="flex space-x-2 p-2 rounded-full bg-[#ffffff33]">
+            <div className="flex space-x-2 p-2">
             {
                 items.map((item, index) => {
-                    return <div key={index} className={`w-3 h-3 rounded-full shadow-md transition-all ${index + 1 == curPage ? 'bg-white' : 'bg-[#ffffff66]'}`}></div>;
+                    return <div key={index} className={`w-2 h-2 rounded-full shadow-md transition-all ${index + 1 == curPage ? 'bg-white' : 'bg-[#ffffff66]'}`}></div>;
                 })                
             }
             </div>
